4671cab3f85530674112cda6e89c8a3f87634de1
Gadget <road.runner.grach@gmail.com>
2018-02-22 02:02:49 +0300

    Add new parameters in ini file. Add logic for read and use new parameter in excel read and write procedure's

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import configparser
 
 config_name = "config.ini"
 
+
 class Record(object):
 
     def __init__(self, name, id, price, count):
@@ -17,16 +18,26 @@ class Record(object):
     def record_id(self):
         return '{0}_{1}'.format(self.id, self.price)
 
-class Exclfile(object):
+
+class Excelfile(object):
 
     def __init__(self, file_name):
         self.file_name = file_name
         # Creale clear variables
         self.dictr = {}
-        self.input_file = ''
-        self.output_file = ''
-        self.data_start = 0
+        # File params
+        self.input_file = None
+        self.output_file = None
+        self.data_start = None
+        # Col params
+        self.col_name = None
+        self.col_id = None
+        self.col_measure = None
+        self.col_price = None
+        self.col_count = None
+        self.col_total = None
 
+        self.read_config()
 
     def read_xls_file(self):
         file_path = '{0}/{1}'.format(os.getcwd(), self.input_file)
@@ -38,19 +49,35 @@ class Exclfile(object):
             #     pass
 
             for i in range(self.data_start, ws.max_row - self.data_start):
-                name = ws.cell(row=i, column=2).value
-                id = ws.cell(row=i, column=3).value
-                price = ws.cell(row=i, column=5).value
-                count = ws.cell(row=i, column=6).value
+
+                if self.col_name != None:
+                    name = ws.cell(row=i, column=self.col_name).value
+                else:
+                    name = ws.cell(row=i, column=2).value
+
+                if self.col_id != None:
+                    id = ws.cell(row=i, column=self.col_id).value
+                else:
+                    id = ws.cell(row=i, column=3).value
+
+                if self.col_price != None:
+                    price = ws.cell(row=i, column=self.col_price).value
+                else:
+                    price = ws.cell(row=i, column=5).value
+
+                if self.col_count != None:
+                    count = ws.cell(row=i, column=self.col_count).value
+                else:
+                    count = ws.cell(row=i, column=6).value
+
                 rec = Record(name, id, price, count)
 
                 rec2 = self.dictr.get(rec.record_id())
-                if rec2 != None :
+                if rec2 != None:
                     rec.count += rec2.count
                     self.dictr.pop(rec2.record_id())
 
                 self.dictr[rec.record_id()] = rec
-
 
     def write_xls_file(self):
         file_path = '{0}/{1}'.format(os.getcwd(), self.output_file)
@@ -59,12 +86,36 @@ class Exclfile(object):
 
         i = self.data_start
         for item in self.dictr.values():
-            sheet.cell(row=i, column=2).value = item.name
-            sheet.cell(row=i, column=3).value = item.id
-            sheet.cell(row=i, column=4).value = u'шт.'
-            sheet.cell(row=i, column=5).value = item.price
-            sheet.cell(row=i, column=6).value = item.count
-            sheet.cell(row=i, column=7).value = '''=MMULT(E{0};F{0})'''.format(i)
+            if self.col_name != None:
+                sheet.cell(row=i, column=self.col_name).value = item.name
+            else:
+                sheet.cell(row=i, column=2).value = item.name
+
+            if self.col_id != None:
+                sheet.cell(row=i, column=self.col_id).value = item.id
+            else:
+                sheet.cell(row=i, column=3).value = item.id
+
+            if self.col_measure != None:
+                sheet.cell(row=i, column=self.col_measure).value = u'шт.'
+            else:
+                sheet.cell(row=i, column=4).value = u'шт.'
+
+            if self.col_price != None:
+                sheet.cell(row=i, column=self.col_price).value = item.price
+            else:
+                sheet.cell(row=i, column=5).value = item.price
+
+            if self.col_count != None:
+                sheet.cell(row=i, column=self.col_count).value = item.price
+            else:
+                sheet.cell(row=i, column=6).value = item.price
+
+            if self.col_measure != None:
+                sheet.cell(row=i, column=self.col_measure).value = item.count * item.price
+            else:
+                sheet.cell(row=i, column=7).value = item.count * item.price
+
             i += 1
 
         book.save(file_path)
@@ -78,11 +129,15 @@ class Exclfile(object):
             # Start from 0, then title_height + 1
             self.data_start = int(conf['FILE']['HeaderRowCount']) + 1
 
+            self.col_name = int(conf['ROWS']['ColName'])
+            self.col_id = int(conf['ROWS']['ColId'])
+            self.col_measure = int(conf['ROWS']['ColMeasure'])
+            self.col_price = int(conf['ROWS']['ColPrice'])
+            self.col_count = int(conf['ROWS']['ColCount'])
+            self.col_total = int(conf['ROWS']['ColTotal'])
 
 
 if __name__ == "__main__":
-    xls = Exclfile(config_name)
-
-    xls.read_config()
+    xls = Excelfile(config_name)
     xls.read_xls_file()
     xls.write_xls_file()
