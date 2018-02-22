bc0d7ee91606949f31dd34e594fdc64db837e5bc
Gadget <road.runner.grach@gmail.com>
2018-02-23 01:09:00 +0300

    Add repr method for class Record. Add procedure for prepare file to write

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 # import pandas as pd
 # from tabulate import tabulate
-from django.contrib.messages.storage import fallback
 from openpyxl import Workbook, load_workbook
 import configparser
-import operator
+from operator import attrgetter
+from shutil import copyfile
 
 config_name = "config.ini"
 
@@ -16,6 +16,9 @@ class Record(object):
         self.count = count
         self.id = id
 
+    def __repr__(self):
+        return repr((self.name, self.id, self.price, self.count))
+
     def record_id(self):
         return '{0}_{1}'.format(self.id, self.price)
 
@@ -26,6 +29,7 @@ class Excelfile(object):
         self.file_name = file_name
         # Creale clear variables
         self.list = []
+        self.header = {}
         # File params
         self.input_file = None
         self.output_file = None
@@ -64,45 +68,62 @@ class Excelfile(object):
                 dict[rec.record_id()] = rec
 
             for value in dict.values():
-                bufflist.append(value)
+                self.list.append(value)
 
-            self.list = sorted(bufflist, key=operator.itemgetter(0))
-
-
+            # self.list.sort(key=attrgetter('name'))
 
     def write_xls_file(self):
+        self.prepare_file_for_write()
         file_path = '{0}/{1}'.format(os.getcwd(), self.output_file)
-        book = Workbook()
-        sheet = book.active
+        if os.path.exists(file_path):
+            wb = load_workbook(filename=file_path)
+            ws = wb.active
 
-        i = self.data_start
-        for item in self.list.values():
-            sheet.cell(row=i, column=self.col_name).value = item.name
-            sheet.cell(row=i, column=self.col_id).value = item.id
-            sheet.cell(row=i, column=self.col_measure).value = u'шт.'
-            sheet.cell(row=i, column=self.col_price).value = item.price
-            sheet.cell(row=i, column=self.col_count).value = item.price
-            sheet.cell(row=i, column=self.col_measure).value = item.count * item.price
+            i = self.data_start
+            for item in self.list.values():
+                ws.cell(row=i, column=self.col_name).value = item.name
+                ws.cell(row=i, column=self.col_id).value = item.id
+                ws.cell(row=i, column=self.col_measure).value = u'шт.'
+                ws.cell(row=i, column=self.col_price).value = item.price
+                ws.cell(row=i, column=self.col_count).value = item.price
+                ws.cell(row=i, column=self.col_measure).value = item.count * item.price
 
-            i += 1
+                i += 1
 
-        book.save(file_path)
+        wb.save(file_path)
 
     def read_config(self):
         if self.file_name.strip() != '':
             conf = configparser.ConfigParser()
             conf.read(self.file_name.strip())
             self.input_file = conf.get('FILE', 'InputFileName', fallback='input.xls')
-            self.output_file = conf('FILE', 'OutputFileName', fallback='output.xls')
+            self.output_file = conf.get('FILE', 'OutputFileName', fallback='output.xls')
             # Start from 0, then title_height + 1
-            self.data_start = conf.get('FILE', 'HeaderRowCount', fallback=1) + 1
+            self.data_start = int(conf.get('FILE', 'HeaderRowCount', fallback='1')) + 1
             #Read row params file
-            self.col_name = conf.get('ROWS', 'ColName', fallback=2)
-            self.col_id = conf.get('ROWS', 'ColId', fallback=3)
-            self.col_measure = conf.get('ROWS', 'ColMeasure', fallback=4)
-            self.col_price = conf.get('ROWS', 'ColPrice', fallback=5)
-            self.col_count = conf.get('ROWS', 'ColCount', fallback=6)
-            self.col_total = conf.get('ROWS', 'ColTotal', fallback=7)
+            self.col_name = int(conf.get('ROWS', 'ColName', fallback='2'))
+            self.col_id = int(conf.get('ROWS', 'ColId', fallback='3'))
+            self.col_measure = int(conf.get('ROWS', 'ColMeasure', fallback='4'))
+            self.col_price = int(conf.get('ROWS', 'ColPrice', fallback='5'))
+            self.col_count = int(conf.get('ROWS', 'ColCount', fallback='6'))
+            self.col_total = int(conf.get('ROWS', 'ColTotal', fallback='7'))
+
+    def prepare_file_for_write(self):
+        # Copy file to destination path
+        copyfile('{0}/{1}'.format(os.getcwd(), self.input_file), '{0}/{1}'.format(os.getcwd(), self.output_file))
+        # Remove all rows except the title
+        if os.path.exists('{0}/{1}'.format(os.getcwd(), self.output_file)):
+            wb = load_workbook(filename='{0}/{1}'.format(os.getcwd(), self.output_file))
+            ws = wb.active
+
+            for rows in range(self.data_start, ws.max_row - self.data_start):
+                if rows[1] == 'None':
+                    rows[0].value == None
+                    rows[1].value == None
+                else:
+                    rows[0].value = rows[0].value + "," + rows[1].value.replace("-", "")
+                    rows[1].value = None
+
 
 
 if __name__ == "__main__":
